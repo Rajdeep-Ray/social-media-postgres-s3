2db2e7eafc6f1bead0170a751423b89336e87c84
Rajdeep Ray <rayrajdeep2001@gmail.com>
2021-09-16 21:02:16 +0530

    update | user model

---
+++ DB/Models/users.js
@@ -0,0 +1,42 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = new require('../config');
+
+const users = sequelize.define('users', {
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    phone: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    profile_pic: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    bio: {
+        type: DataTypes.STRING
+    },
+    role: {
+        type: DataTypes.ARRAY(DataTypes.ENUM(['anonymous', 'user', 'super_admin'])),
+        defaultValue:['user']
+    }
+
+})
+
+users.sync({ force: false }).then(() => console.log("`users` table created"))
+
+module.exports = users;
