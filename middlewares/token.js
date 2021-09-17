50f8041b4a57c7c8b0583fd3bfe1470027355d32
Rajdeep Ray <rayrajdeep2001@gmail.com>
2021-09-17 12:35:17 +0530

    update

--- middlewares/token.js
+++ middlewares/token.js
@@ -1,10 +1,19 @@
 const jwt = require('jsonwebtoken');
 
 exports.token = (obj) => {
-    const tok = jwt.sign(obj,'HelloWorld')
+    const tok = jwt.sign(obj, 'HelloWorld')
     return tok;
 }
 
-exports.verify = () => {
-    //verify token
+exports.verify = (req, res, next) => {
+    const bearerHeader = req.headers['authorization'];
+    if (typeof bearerHeader !== 'undefined') {
+        const bearer = bearerHeader.split(' ');
+        const bearerToken = bearer[1];
+        req.token = jwt.verify(bearerToken, 'HelloWorld');
+        next();
+    } else {
+        res.sendStatus(403);
+    }
+
 }
