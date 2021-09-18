536c5100ae940c2401343abaf56d55f0f4f24a8a
Rajdeep Ray <rayrajdeep2001@gmail.com>
2021-09-19 02:13:36 +0530

    Update

--- routes/users.js
+++ routes/users.js
@@ -19,4 +19,36 @@ userRouter.get('/:id', function (req, res, next) {
 		})
 });
 
+userRouter.post('/makeAdmin/:id', (req, res, next) => {
+	db.Users.findByPk(req.params.id)
+		.then((user) => {
+			if (user.role.includes('super_admin')) {
+				res.send("Already ADMIN");
+			}
+			var _roles = user.role;
+			_roles.push('super_admin')
+
+			db.Users.update({
+				role: _roles
+			}, {
+				where: {
+					id: req.params.id
+				}
+			})
+				.then(() => {
+					res.send("USER ROLE Updated!")
+				})
+				.catch((err) => {
+					console.log(err.message);
+					res.status(500).send(err.message)
+				})
+		})
+		.catch((err) => {
+			console.log(err.message);
+			res.send("USER not found!")
+		})
+
+})
+
+
 module.exports = userRouter;
